044f4d6a2ffbf0e4ce49d2bb22f8933a5e19d468
patriciacolaiacovo <patriciacolaiacovo@gmail.com>
2018-06-08 22:05:15 -0400

    first commit

---
+++ js/homework.js
@@ -0,0 +1,36 @@
+//select you elements or define your variables
+
+
+
+
+
+
+//define your functions
+function solarSign(s){
+     var object = {
+         Virgo: "is espectacular",
+         Gemini: "great",
+         Sagittarius: "a",
+         Capricorn: "b",
+         Cancer: "c",
+         Scorpion: "d"
+     }
+     return 
+}
+
+
+
+//call you functions and/or add event listeners
+main.innerHTML+=
+`
+<select id="solarSign">
+<option value="virgo">Virgo</option>
+<option value="scorpion">Scorpion</option>
+<option value="sagittarius">Sagittarius</option>
+<option value="cancer">Cancer</option>
+</select>
+
+`
+
+
+
